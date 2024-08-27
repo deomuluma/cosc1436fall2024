3f8a6fcad505614165f64807e17ee8b905034641
deo mulumba <deo.mulumba@my.tccd.edu>
2024-08-26 21:51:03 -0500

    chatre2

---
+++ classwork/chaptre2/variableAndtypers/variableAndtypers.cpp
@@ -0,0 +1,45 @@
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to Fi variableAndtypers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //output a message - 1
+    std::cout << "hello world" << std::endl;
+    //message -2
+    std::cout << "hello" << "  " << "world" << std::endl;
+
+    //message - 3
+    std::cout << "hello";
+    std::cout << " ";
+    std::cout << "hello";
+    std::cout << std::endl;
+
+    //message -4
+    std::cout << "hello world\n";
+
+    std::cout << "hello \Bob\"";
+
+
+    //file paths
+    std::cout << "C:windows\temp";
+    //std::cout << R"C:\windows\temp";
+
+
+    //variables - named memory location to store data
+    // Scope - lifetime of  a variable/identifier: point of declaration to end of block
+
+    //Declaration - tell compiler a variable exists
+    int length;
+    length = 0;
+    int releaseyear = 1900;
+    int reting = 3, stars = 5;
+
+
+    std::cout << length;
+
+}
+        
+
