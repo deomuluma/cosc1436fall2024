b2cf59ee5a533b298e2df369ded4d1fe393ba304
deo mulumba <deo.mulumba@my.tccd.edu>
2024-09-11 20:56:39 -0500

    hello

--- classwork/chaptre3/chaptre3.cpp
+++ classwork/chaptre3/chaptre3.cpp
@@ -1,6 +1,11 @@
 // chapter3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
 #include <string>
+#include <cmath> //C math functions
+#include <iomanip> //Formatted output
+
+//Include the std namespace so we don't have to prefix it on everything
+using namespace std;
 
 
 int main()
@@ -25,69 +30,157 @@ int main()
 
 
 //Get user's name
-std::string name;
-std::cout << "Enter your name: ";
+    std::string name;
+    std::cout << "Enter your name: ";
 
-// std::cin >> V
-// Inserts value up to first whitespace into variable
-// Doesn't work with spaced strings
-//std::cin >> name;
-std::getline(std::cin, name);
+    // std::cin >> V
+    // Inserts value up to first whitespace into variable
+    // Doesn't work with spaced strings
+    //std::cin >> name;
+    std::getline(std::cin, name);
 
-std::cout << "Hello " << name << std::endl;
+    std::cout << "Hello " << name << std::endl;
 
-int hours;
-std::cout << "How many hours did you work this week? ";
-std::cin >> hours;
+    int hours;
+    std::cout << "How many hours did you work this week? ";
+    std::cin >> hours;
 
-double payRate;
-std::cout << "How much do you earn an hour? ";
-std::cin >> payRate;
+    double payRate;
+    std::cout << "How much do you earn an hour? ";
+    std::cin >> payRate;
 
-std::cout << "You earned $" << (hours * payRate) << " this week" << std::endl;
+    std::cout << "You earned $" << (hours * payRate) << " this week" << std::endl;
 
-//Multi-value input, values are whitespace separated
-int x, y;
-std::cout << "Enter the X,Y values of a point: ";
-std::cin >> x >> y;
-
-
-//Type coercion - Compiler changes type of expression
-// No impact on runtime code generation
-    // Always safe
-    // Only impacts current expression
-    // Type hierarchy
-    //  //   float
-    //   unsigned long
-    //   long
-    // //   unsigned int
-    //   int
-    //   char/short/unsigned short (automatic to int)
-
-    // double = int * double
-    // double = double * double (type coercion)
-    // double = double
-double result = hours * payRate;
+    //Multi-value input, values are whitespace separated
+    int x, y;
+    std::cout << "Enter the X,Y values of a point: ";
+    std::cin >> x >> y;
 
 
-//Precedence rules don't apply, just find the largest type
-    // double + float * int => double
+    //Type coercion - Compiler changes type of expression
+    // No impact on runtime code generation
+        // Always safe
+        // Only impacts current expression
+        // Type hierarchy
+        //  //   float
+        //   unsigned long
+        //   long
+        // //   unsigned int
+        //   int
+        //   char/short/unsigned short (automatic to int)
 
-    //Assignment doesn't matter, types determined on each type of eeach side of operator
-    // double = int * float
+        // double = int * double
+        // double = double * double (type coercion)
+        // double = double
+    double result = hours * payRate;
 
 
- //Overflow and underflow
-short smallValue = 32767 + 1; //Overflow
-std::cout << smallValue << std::endl;
+    //Precedence rules don't apply, just find the largest type
+        // double + float * int => double
 
-short largeValue = -32768 - 1; //Underflow
-std::cout << largeValue << std::endl;
+        //Assignment doesn't matter, types determined on each type of eeach side of operator
+        // double = int * float
 
 
-float floatValue = 1e38 * 2e20;
-std::cout << floatValue << std::endl;
+     //Overflow and underflow
+    short smallValue = 32767 + 1; //Overflow
+    std::cout << smallValue << std::endl;
+
+    short largeValue = -32768 - 1; //Underflow
+    std::cout << largeValue << std::endl;
+
+
+    float floatValue = 1e38 * 2e20;
+    std::cout << floatValue << std::endl;
+
+
+     //Typecasting - you convert an expression to another type explicitly
+    int totalSales = 50000;
+    int departments = 8;
+
+     // static_cast<T>(E) => preferred
+    //   // (T)E => C-style, not recommended
+    //   Must be allowable
+    //double averageSalesPerDepartment = static_cast<double>(totalSales) / departments;
+    double averageSalesPerDepartment = totalSales / static_cast<double>(departments);
+     //Compiler error, cannot convert from string to int
+    std::string someString = "Hello";
+    //int someStringNumber = static_cast<int>(someString);
+     //Use cast to truncated data w/o compiler warning
+    int truncatedValue = static_cast<int>(floatValue);
+
+      //Math functions
+    // pow(x, y) => x to the y power
+    // sqrt(x) => square root of x
+    // exp(x) => e to the x power
+    // log(x) => log of x  log(exp(x)) = x
+    // sin(x), cos(x), tan(x), cot(x), sec(x), csc(x), ...h
+    // abs(x) => positive value of x
+    double xValue = 45.6;
+    double mathResult;
+
+    std::cout << pow(xValue, 2);  //xValue * xValue
+    std::cout << sqrt(xValue);
+
+    std::cout << exp(2);  // e nth power, e = 2.718
+    std::cout << log(5.4);  // root of e
+
+
+    std::cout << abs(-45);  // positive X = 45
+
+    std::cout << ceil(xValue);     // 46
+    std::cout << floor(xValue);    // 45
+
+    std::cout << trunc(xValue);
+    std::cout << round(xValue);   // 46
+
+    //Formatted output
+    std::cout << 4.567891241415151 << std::endl;
+    std::cout << 5.67e3 << std::endl;
+
+     // Create a table
+    // Manipulator | behavior | Persist
+    // --------
+    //  setw(i) | pads the value to i | No
+    //  left | Left justifies | Yes
+    //  right | Right justifies (default) | Yes
+    
+    cout << left << setw(8) << "Student" << setw(6) << "Grade" << setw(6) << "Letter" << endl;
+    cout << setw(20) << setfill('-') << "" << setfill(' ') << endl;
+    //cout << fixed;
+    cout << setprecision(2);
+    cout << setw(8) << "Bob" << setw(6) << 95.67 << "A" << endl;
+    cout << setw(8) << "Sue" << setw(6) << 98.543 << "A" << endl;
+    cout << setw(8) << "Jim" << setw(6) << 84.567 << "B" << endl;
+    cout << setw(8) << "Jan" << setw(6) << 78.987 << "C" << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
 
 
